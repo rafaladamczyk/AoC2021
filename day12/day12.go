ff33a58ec6a22585edc367fe992d937b077ee4e4
Rafał Adamczyk <rafal.adamczyk@gmail.com>
2023-07-27 23:33:07 +0200

    day12

---
+++ day12/day12.go
@@ -0,0 +1,175 @@
+package day12
+
+import (
+	"fmt"
+	. "raf/aoc2021/inputFetcher"
+	"strings"
+)
+
+type Node struct {
+	destinations []*Node
+	name         string
+	isBig        bool
+}
+
+func (src *Node) AddEdge(dest *Node) {
+	src.destinations = append(src.destinations, dest)
+}
+
+func Run() (string, string) {
+	//input := GetExampleInput()
+	input := GetInput(12)
+	return part1(input), part2(input)
+}
+
+func part1(lines []string) string {
+	m := buildMap(lines)
+	start := m["start"]
+	end := m["end"]
+
+	paths := findAllPaths(start, end, make([]*Node, 0, 1000))
+	// for _, p := range paths {
+	// 	printPath(p)
+	// }
+	return fmt.Sprintf("%d", len(paths))
+}
+
+func part2(lines []string) string {
+	m := buildMap(lines)
+	start := m["start"]
+	end := m["end"]
+
+	smalls := make([]*Node, 0)
+	for _, n := range m {
+		if !n.isBig && n.name != "start" {
+			smalls = append(smalls, n)
+		}
+	}
+
+	normalPaths := findAllPaths(start, end, make([]*Node, 0, 1000))
+	// for _, p := range normalPaths {
+	// 	printPath(p)
+	// }
+
+	acc := 0
+	for _, small := range smalls {
+		for _, path := range findAllPaths2(start, end, make([]*Node, 0, 1000), small.name) {
+			if count(path, small) == 2 {
+				// printPath(path)
+				acc++
+			}
+		}
+	}
+
+	return fmt.Sprintf("%d", len(normalPaths)+acc)
+}
+func findAllPaths(start, end *Node, visited []*Node) [][]*Node {
+	if start == end {
+		return [][]*Node{{end}}
+	}
+
+	if !start.isBig {
+		visited = append(visited, start)
+	}
+
+	paths := make([][]*Node, 0)
+
+	for _, dest := range start.destinations {
+		if dest.isBig || !contains(visited, dest) {
+			v := make([]*Node, len(visited))
+			copy(v, visited)
+			results := findAllPaths(dest, end, v)
+			for _, path := range results {
+				newPath := make([]*Node, 0, len(path)+1)
+				newPath = append(newPath, start)
+				newPath = append(newPath, path...)
+				paths = append(paths, newPath)
+			}
+		}
+	}
+
+	return paths
+}
+
+func findAllPaths2(start, end *Node, visited []*Node, small string) [][]*Node {
+	if start == end {
+		return [][]*Node{{end}}
+	}
+
+	if !start.isBig {
+		visited = append(visited, start)
+	}
+
+	paths := make([][]*Node, 0)
+
+	for _, dest := range start.destinations {
+		visitedCount := count(visited, dest)
+		if dest.isBig || visitedCount == 0 || (dest.name == small && visitedCount == 1) {
+			v := make([]*Node, len(visited))
+			copy(v, visited)
+			for _, path := range findAllPaths2(dest, end, v, small) {
+				newPath := make([]*Node, 0, len(path)+1)
+				newPath = append(newPath, start)
+				newPath = append(newPath, path...)
+				paths = append(paths, newPath)
+			}
+		}
+	}
+
+	return paths
+}
+
+func contains(nodes []*Node, node *Node) bool {
+	for _, n := range nodes {
+		if n == node {
+			return true
+		}
+	}
+	return false
+}
+
+func count(nodes []*Node, node *Node) int {
+	acc := 0
+	for _, n := range nodes {
+		if n == node {
+			acc++
+		}
+	}
+	return acc
+}
+
+func buildMap(lines []string) map[string]*Node {
+	m := make(map[string]*Node)
+
+	for _, l := range lines {
+		s := strings.Split(l, "-")
+		first := getOrCreateNode(m, s[0])
+		second := getOrCreateNode(m, s[1])
+		first.AddEdge(second)
+		second.AddEdge(first)
+	}
+
+	return m
+}
+
+func getOrCreateNode(m map[string]*Node, name string) *Node {
+	n := m[name]
+	if n == nil {
+		newNode := &Node{name: name, destinations: make([]*Node, 0), isBig: isBigCavern(name)}
+		m[name] = newNode
+		return newNode
+	} else {
+		return n
+	}
+}
+
+func isBigCavern(s string) bool {
+	return strings.ToUpper(s) == s
+}
+
+func printPath(p []*Node) {
+	for _, n := range p {
+		fmt.Printf("%s -> ", n.name)
+	}
+	fmt.Println()
+}
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"raf/aoc2021/day10"
 	"raf/aoc2021/day11"
+	"raf/aoc2021/day12"
 	"raf/aoc2021/day9"
 	"time"
 )
@@ -24,6 +25,10 @@ func main() {
 	ans1, ans2 = day11.Run()
 	fmt.Printf("Day 11 Part 1: %s\n", ans1)
 	fmt.Printf("Day 11 Part 2: %s\n", ans2)
+
+	ans1, ans2 = day12.Run()
+	fmt.Printf("Day 12 Part 1: %s\n", ans1)
+	fmt.Printf("Day 12 Part 2: %s\n", ans2)
 
 	elapsed := time.Since(start)
 	fmt.Printf("\nFinished in %s", elapsed)
